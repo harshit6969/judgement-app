65c04534924f9f6b0cfb0b30942e37afbbf74fea
Harshit <hshrivastava1996@gmail.com>
2023-10-02 08:28:53 +0530

    Update build

---
+++ build/precache-manifest.b4655284dad2351bde9e487157adf49f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bebd6cd0d1d6dbcd2287416f614d6d2",
+    "url": "/judgement-app/index.html"
+  },
+  {
+    "revision": "4a64f92f27db4de41934",
+    "url": "/judgement-app/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "1d56be925a36a3650c40",
+    "url": "/judgement-app/static/css/main.f9280981.chunk.css"
+  },
+  {
+    "revision": "4a64f92f27db4de41934",
+    "url": "/judgement-app/static/js/2.c169e56e.chunk.js"
+  },
+  {
+    "revision": "1d56be925a36a3650c40",
+    "url": "/judgement-app/static/js/main.228de954.chunk.js"
+  },
+  {
+    "revision": "e6ca38aa13d5163e3edb",
+    "url": "/judgement-app/static/js/runtime~main.3d33b236.js"
+  },
+  {
+    "revision": "a15bf8da108e4e78e347f763ada17938",
+    "url": "/judgement-app/static/media/Harshit.a15bf8da.jpg"
+  },
+  {
+    "revision": "8ad40e89e8e58031d31b303b20f48368",
+    "url": "/judgement-app/static/media/Maadi.8ad40e89.jpg"
+  },
+  {
+    "revision": "b6aad3d5a9b292dba19442fb9c913640",
+    "url": "/judgement-app/static/media/Parmar.b6aad3d5.jpg"
+  },
+  {
+    "revision": "dd0e4a662fad72816473e170f2249908",
+    "url": "/judgement-app/static/media/Shanky.dd0e4a66.jpg"
+  },
+  {
+    "revision": "783a3bfc7467d10dff4c83da0942c7b1",
+    "url": "/judgement-app/static/media/Vikas.783a3bfc.jpg"
+  }
+]);

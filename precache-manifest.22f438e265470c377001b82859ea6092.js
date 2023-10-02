a4c5a7d653199b07bc0c59dbbe96a12633f91dd6
harshit6969 <harshit6969@users.noreply.github.com>
2023-10-02 03:16:41 +0000

    deploy: 3153e818675dfdca1823102d060b95244e7225ad

---
+++ precache-manifest.22f438e265470c377001b82859ea6092.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bd6babe4eb475c6e8edbd2306be3963",
+    "url": "/judgement-app/index.html"
+  },
+  {
+    "revision": "7d9175dec9e135e5e308",
+    "url": "/judgement-app/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "a1a4948a2a24ed85077a",
+    "url": "/judgement-app/static/css/main.f9280981.chunk.css"
+  },
+  {
+    "revision": "7d9175dec9e135e5e308",
+    "url": "/judgement-app/static/js/2.b2fba284.chunk.js"
+  },
+  {
+    "revision": "a1a4948a2a24ed85077a",
+    "url": "/judgement-app/static/js/main.c3f412ea.chunk.js"
+  },
+  {
+    "revision": "e6ca38aa13d5163e3edb",
+    "url": "/judgement-app/static/js/runtime~main.3d33b236.js"
+  },
+  {
+    "revision": "a15bf8da108e4e78e347f763ada17938",
+    "url": "/judgement-app/static/media/Harshit.a15bf8da.jpg"
+  },
+  {
+    "revision": "8ad40e89e8e58031d31b303b20f48368",
+    "url": "/judgement-app/static/media/Maadi.8ad40e89.jpg"
+  },
+  {
+    "revision": "b6aad3d5a9b292dba19442fb9c913640",
+    "url": "/judgement-app/static/media/Parmar.b6aad3d5.jpg"
+  },
+  {
+    "revision": "dd0e4a662fad72816473e170f2249908",
+    "url": "/judgement-app/static/media/Shanky.dd0e4a66.jpg"
+  },
+  {
+    "revision": "783a3bfc7467d10dff4c83da0942c7b1",
+    "url": "/judgement-app/static/media/Vikas.783a3bfc.jpg"
+  }
+]);

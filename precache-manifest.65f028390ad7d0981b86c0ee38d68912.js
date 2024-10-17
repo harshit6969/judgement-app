8b6533526a0827bffce366faaab2735d84d2cb17
harshit6969 <harshit6969@users.noreply.github.com>
2024-10-17 15:46:02 +0000

    deploy: efdae1a5501a7d63e5479820934c9f07951b1a39

---
+++ precache-manifest.65f028390ad7d0981b86c0ee38d68912.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71312274aeb552c7d27d3946f3846f47",
+    "url": "/Judgement/index.html"
+  },
+  {
+    "revision": "b0f03676041f78781da6",
+    "url": "/Judgement/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "27e13118c9208fe13ea8",
+    "url": "/Judgement/static/css/main.f9280981.chunk.css"
+  },
+  {
+    "revision": "b0f03676041f78781da6",
+    "url": "/Judgement/static/js/2.a93c5639.chunk.js"
+  },
+  {
+    "revision": "27e13118c9208fe13ea8",
+    "url": "/Judgement/static/js/main.9d80cfb4.chunk.js"
+  },
+  {
+    "revision": "d63fcbc1842aa67327ae",
+    "url": "/Judgement/static/js/runtime~main.ca3a988b.js"
+  },
+  {
+    "revision": "9bea3725f035a1de9a73f64d0a6ffc43",
+    "url": "/Judgement/static/media/Baby.9bea3725.jpg"
+  },
+  {
+    "revision": "a15bf8da108e4e78e347f763ada17938",
+    "url": "/Judgement/static/media/Harshit.a15bf8da.jpg"
+  },
+  {
+    "revision": "8ad40e89e8e58031d31b303b20f48368",
+    "url": "/Judgement/static/media/Maadi.8ad40e89.jpg"
+  },
+  {
+    "revision": "179b520260b5d8f42d60cb72c1288431",
+    "url": "/Judgement/static/media/Mudit.179b5202.jpg"
+  },
+  {
+    "revision": "b6aad3d5a9b292dba19442fb9c913640",
+    "url": "/Judgement/static/media/Parmar.b6aad3d5.jpg"
+  },
+  {
+    "revision": "dd0e4a662fad72816473e170f2249908",
+    "url": "/Judgement/static/media/Shanky.dd0e4a66.jpg"
+  },
+  {
+    "revision": "a5f300070358bffb34d2d4d7b7d5c42a",
+    "url": "/Judgement/static/media/Shivi.a5f30007.jpg"
+  },
+  {
+    "revision": "783a3bfc7467d10dff4c83da0942c7b1",
+    "url": "/Judgement/static/media/Vikas.783a3bfc.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/judgement-app/precache-manifest.22e8a8cb30e8f437b68ff7bbbcaf7829.js"
+  "/Judgement/precache-manifest.65f028390ad7d0981b86c0ee38d68912.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/judgement-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Judgement/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });

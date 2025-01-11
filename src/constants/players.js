aea2283e313cee62e23d12c7b7db09b9f552ac9f
arushi_sharma <hsh_asharma@contractor.indeed.com>
2025-01-11 18:14:11 +0530

    Add players ( info + update DP)

--- src/constants/players.js
+++ src/constants/players.js
@@ -1,80 +1,114 @@
-import Parmar from './images/Parmar.jpg';
-import Maadi from './images/Maadi.jpg'
-import Harshit from './images/Harshit.jpg'
-import Shanky from './images/Shanky.jpg'
-import Vikas from './images/Vikas.jpg'
-import Shivam from './images/Shivam.jpeg'
-import Billu from './images/Billu.jpeg'
+import Arushi from "./images/Arushi.jpg";
+import Billu from "./images/Billu.jpeg";
+import Harshit from "./images/Harshit.jpg";
+import Maadi from "./images/Maadi.jpg";
+import Parmar from "./images/Parmar.jpg";
+import Piddu from "./images/Piddu.jpg";
+import Santru from "./images/Santru.JPG";
+import Shanky from "./images/Shanky.jpg";
+import Shivam from "./images/Shivam.jpeg";
+import Vikas from "./images/Vikas.jpg";
 
 const players = [
-    {
-        "ID": 0,
-        "Name": "Parmar",
-        "IsMF": true,
-        "Profile": Parmar,
-        "CurrentRoundScore": 0,
-        "TotalScore": 0,
-        Scores: [],
-        "ColorCode": "#f06292"
-    },
-    {
-        "ID": 1,
-        "Name": "Sanket",
-        "IsMF": true,
-        "Profile": Shanky,
-        "CurrentRoundScore": 0,
-        "TotalScore": 0,        Scores: [],
-        "ColorCode": "#B89A43"
-    },
-    {
-        "ID": 2,
-        "Name": "Harshit",
-        "IsMF": false,
-        "Profile": Harshit,
-        "CurrentRoundScore": 0,
-        "TotalScore": 0,
-        Scores: [],
-        "ColorCode": "#1976d2"
-    },
-    {
-        "ID": 3,
-        "Name": "Maadi",
-        "IsMF": false,
-        "Profile": Maadi,
-        "CurrentRoundScore": 0,
-        "TotalScore": 0,
-        Scores: [],
-        "ColorCode": "#2e7d32"
-    },
-    {
-        "ID": 6,
-        "Name": "Shivam",
-        "IsMF": false,
-        "Profile": Shivam,
-        "CurrentRoundScore": 0,
-        "TotalScore": 0,
-        Scores: [],
-        "ColorCode": "#F205FF"
-    },
-    {
-        "ID": 8,
-        "Name": "Vikas",
-        "IsMF": false,
-        "Profile": Vikas,
-        "CurrentRoundScore": 0,
-        "TotalScore": 0,
-        Scores: [],
-        "ColorCode": "#581428"
-    },
-    {
-        "ID": 9,
-        "Name": "Player 1",
-        "IsMF": false,
-        "Profile": Billu,
-        "CurrentRoundScore": 0,
-        "TotalScore": 0,
-        Scores: [],
-        "ColorCode": "#28B5CD"
-    }
-]
+  {
+    ID: 0,
+    Name: "Parmar",
+    IsMF: true,
+    Profile: Parmar,
+    CurrentRoundScore: 0,
+    TotalScore: 0,
+    Scores: [],
+    ColorCode: "#f06292",
+  },
+  {
+    ID: 1,
+    Name: "Sanket",
+    IsMF: true,
+    Profile: Shanky,
+    CurrentRoundScore: 0,
+    TotalScore: 0,
+    Scores: [],
+    ColorCode: "#B89A43",
+  },
+  {
+    ID: 2,
+    Name: "Harshit",
+    IsMF: false,
+    Profile: Harshit,
+    CurrentRoundScore: 0,
+    TotalScore: 0,
+    Scores: [],
+    ColorCode: "#1976d2",
+  },
+  {
+    ID: 3,
+    Name: "Maadi",
+    IsMF: false,
+    Profile: Maadi,
+    CurrentRoundScore: 0,
+    TotalScore: 0,
+    Scores: [],
+    ColorCode: "#2e7d32",
+  },
+  {
+    ID: 6,
+    Name: "Shivam",
+    IsMF: false,
+    Profile: Shivam,
+    CurrentRoundScore: 0,
+    TotalScore: 0,
+    Scores: [],
+    ColorCode: "#F205FF",
+  },
+  {
+    ID: 8,
+    Name: "Vikas",
+    IsMF: false,
+    Profile: Vikas,
+    CurrentRoundScore: 0,
+    TotalScore: 0,
+    Scores: [],
+    ColorCode: "#581428",
+  },
+  {
+    ID: 9,
+    Name: "Arushi",
+    IsMF: false,
+    Profile: Arushi,
+    CurrentRoundScore: 0,
+    TotalScore: 0,
+    Scores: [],
+    ColorCode: "#af7ac5",
+  },
+  {
+    ID: 10,
+    Name: "Santru",
+    IsMF: false,
+    Profile: Santru,
+    CurrentRoundScore: 0,
+    TotalScore: 0,
+    Scores: [],
+    ColorCode: "#f39c12",
+  },
+  {
+    ID: 11,
+    Name: "Lawrence",
+    IsMF: false,
+    Profile: Billu,
+    CurrentRoundScore: 0,
+    TotalScore: 0,
+    Scores: [],
+    ColorCode: "#28B5CD",
+  },
+  {
+    ID: 12,
+    Name: "Piddu",
+    IsMF: false,
+    Profile: Piddu,
+    CurrentRoundScore: 0,
+    TotalScore: 0,
+    Scores: [],
+    ColorCode: "#839192",
+  },
+];
 export default players;

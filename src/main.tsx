7f4a64d49e7cf9ad2878b2308774bcb04588c977
Harshit Shrivastava <38683222+harshit6969@users.noreply.github.com>
2025-04-19 20:30:33 +0530

    fix strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { GlobalErrorBoundary, NotificationCenter } from "./components/GlobalErrorBoundary.tsx";
+import {
+  GlobalErrorBoundary,
+  NotificationCenter,
+} from "./components/GlobalErrorBoundary.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  // <StrictMode>
-  <GlobalErrorBoundary>
-    <NotificationCenter />
-    <App />
-  </GlobalErrorBoundary>
-  // </StrictMode>,
+  <StrictMode>
+    <GlobalErrorBoundary>
+      <NotificationCenter />
+      <App />
+    </GlobalErrorBoundary>
+  </StrictMode>
 );
